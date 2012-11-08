36a8572699bf4f2601238565fb35bf0fea03d1d1
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-11-08 10:49:24 -0700

    Getting closer to finishing refactoring...

--- src/com/BudgetBoss/main/BudgetBoss.java
+++ src/com/BudgetBoss/main/BudgetBoss.java
@@ -1,83 +1,31 @@
 package com.BudgetBoss.main;
 
-import java.io.File;
-
 public class BudgetBoss {
 	
-	static InputListener listener;
 	static Prompter prompter = new Prompter();
+	static PrompterLogic logicChecker = new PrompterLogic();
 	static Budget currentBudget = null;
-	static InputValidator validator;
-	static BudgetFinder finder = new BudgetFinder();
-	
-	public static String getInput(){
-		listener = new InputListener();
-		validator = new InputValidator();
-		String userInput = listener.listenForInput();
-		return userInput;
-		}
-	
-	public static void askToOpenBudget(String defaultDirectory, boolean promptCleared, boolean budgetExists){
-		File[] foundBudgets;
-		String validatedInput = validator.inputIsEitherYOrN(getInput());
-		if(validatedInput.equals("y")){
-			foundBudgets = finder.findBudgets(defaultDirectory);
-			finder.printFoundBudgets(foundBudgets);
-			Prompter.openBudgetPrompt();
-			promptCleared = true;
-			budgetExists = true;
-		}
-		else if(validatedInput.equals("n")){
-			Prompter.dontSearchBudgets();
-			promptCleared = true;
-		}
-		else
-			Prompter.invalidEntryPromptYOrN();
-	}
-	
-	public static void askToCreateBudget(boolean budgetExists){
-		Prompter.createNewBudgetPrompt();
-		String validatedInput = validator.inputIsEitherYOrN(getInput());
-		if(validatedInput.equals("y")){
-			Prompter.getBudgetNamePrompt();
-			String desiredName = getInput();
-			currentBudget = new Budget(desiredName);
-			Prompter.showDesiredBudgetName(desiredName);
-			budgetExists = true;
-		}
-		else if(validatedInput.equals("n")){
-			Prompter.fuckThisProgram();
-			budgetExists = true;
-		}
-		else
-			Prompter.invalidEntryPromptYOrN();
-	}
-	
+		
 	public static void main(String args[]){
-		String validatedInput;
-		File[] foundBudgets;
 		String defaultDirectory = "/home/visionary/Programming/training/BudgetBoss/SavedBudgets/";
-		boolean openPromptCleared = false;
-		boolean newPromptCleared = false;
-		boolean budgetExists = false;
 		
 		System.out.println("Welcome to BudgetBoss!");
 		System.out.println("Manage your money...like a boss.");
 		System.out.println("Is there an existing Budget you want to open? (y/n)");
 		
-		while(!openPromptCleared)
-			askToOpenBudget(defaultDirectory, openPromptCleared, budgetExists);
+		while(!logicChecker.getPromptClearedStatus())
+				logicChecker.askToOpenBudget(defaultDirectory);
 		
-		
-		while(!budgetExists){
-			askToCreateBudget(budgetExists);
-		}
+		while(!logicChecker.getBudgetExistsStatus()){
+			currentBudget = logicChecker.askToCreateBudget();
 		
 		Salvation savior = new Salvation();
 		try{	
 			savior.saveOnExit(currentBudget.getName(), currentBudget);
 		}catch (NullPointerException e){
-		System.out.println("No Budget to save!");
+			System.out.println("No Budget to save!");
+			}
 		}
 	}
 }
+
---
+++ src/com/BudgetBoss/main/PrompterLogic.java
@@ -0,0 +1,74 @@
+package com.BudgetBoss.main;
+
+import java.io.File;
+
+public class PrompterLogic {
+	
+	static boolean openPromptCleared = false;
+	static boolean budgetExists = false;
+	
+	private void openPromptCleared(){
+		openPromptCleared = true;
+	}
+	
+	public boolean getPromptClearedStatus(){
+		return openPromptCleared;
+	}
+	
+	private void budgetExists(){
+		budgetExists = true;
+	}
+	
+	public boolean getBudgetExistsStatus(){
+		return budgetExists;
+	}
+	
+	public static String getInput(){
+		InputListener listener = new InputListener();
+		String userInput = listener.listenForInput();
+		return userInput;
+		}
+	
+	public void askToOpenBudget(String defaultDirectory){
+		File[] foundBudgets;
+		InputValidator validator = new InputValidator();
+		BudgetFinder finder = new BudgetFinder();
+		String validatedInput = validator.inputIsEitherYOrN(getInput());
+		if(validatedInput.equals("y")){
+			foundBudgets = finder.findBudgets(defaultDirectory);
+			finder.printFoundBudgets(foundBudgets);
+			Prompter.openBudgetPrompt();
+			openPromptCleared();
+			budgetExists();
+		}
+		else if(validatedInput.equals("n")){
+			Prompter.dontSearchBudgets();
+			openPromptCleared();		
+		}
+		else
+			Prompter.invalidEntryPromptYOrN();
+	}
+	
+	public Budget askToCreateBudget(){
+		Prompter.createNewBudgetPrompt();
+		InputValidator validator = new InputValidator();
+		String validatedInput = validator.inputIsEitherYOrN(getInput());
+		if(validatedInput.equals("y")){
+			Prompter.getBudgetNamePrompt();
+			String desiredName = getInput();
+			Budget currentBudget = new Budget(desiredName);
+			Prompter.showDesiredBudgetName(desiredName);
+			budgetExists();
+			return currentBudget;
+		}
+		else if(validatedInput.equals("n")){
+			Prompter.fuckThisProgram();
+			budgetExists();
+			return null;
+		}
+		else
+			Prompter.invalidEntryPromptYOrN();
+		return null;
+	}
+
+}

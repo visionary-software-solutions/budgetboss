27ba6c16fea99b7840f1e81c3aeb2a4097ed68d1
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-11-15 10:58:09 -0700

    Passed first ExpenseCategory test

--- src/com/VSSBudgetBoss/budget/ExpenseCategory.java
+++ src/com/VSSBudgetBoss/budget/ExpenseCategory.java
@@ -6,9 +6,24 @@ import java.util.ArrayList;
 public class ExpenseCategory {
 	
 	private String categoryName;
-	private ArrayList<Expense> expenses;
+	private ArrayList<Expense> expenses = new ArrayList<Expense>();
 	private BigDecimal budgeted;
 	private BigDecimal totalSpent;
 	private BigDecimal totalRemaining;
-
+	
+	public ExpenseCategory(String name){
+		categoryName = name;
+	}
+	
+	public void addExpense(Expense expense){
+		expenses.add(expense);
+	}
+	
+	public BigDecimal getSpent(){
+		totalSpent = new BigDecimal("00.00");
+		for(int i = 0; i < expenses.size(); i++){
+			totalSpent = totalSpent.add(expenses.get(i).getCost());
+		}
+		return totalSpent;
+	}
 }

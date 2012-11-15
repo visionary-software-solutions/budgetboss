e52a2323991e640f9de59ea59d35abd0340edcd9
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-11-15 10:14:00 -0700

    Started on Expense/ExpenseCategory, Ubuntu is bugging to restart from updates

--- src/com/VSSBudgetBoss/budget/ExpenseCategory.java
+++ src/com/VSSBudgetBoss/budget/ExpenseCategory.java
@@ -1,5 +1,14 @@
 package com.VSSBudgetBoss.budget;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class ExpenseCategory {
+	
+	private String categoryName;
+	private ArrayList<Expense> expenses;
+	private BigDecimal budgeted;
+	private BigDecimal totalSpent;
+	private BigDecimal totalRemaining;
 
 }

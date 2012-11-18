4497368ba99ff5eb904ddd927ddad976ed4e4985
Nathan Hutcheson <nathanuhutcheson@gmail.com>
2012-11-17 17:26:34 -0700

    Added test case for Budginator conversion

---
+++ tests/com/VSSBudgetBoss/tests/BudgetToJSONTests.java
@@ -0,0 +1,22 @@
+package com.VSSBudgetBoss.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.visionarysoftwaresolutions.budgetboss.budget.Budget;
+import com.visionarysoftwaresolutions.budgetboss.budginator.BudgetToJSON;
+
+public class BudgetToJSONTests {
+
+	@Test
+	public void testConversion() {
+		//Given: A budget created by BudgetBoss
+		Budget testBudget = new Budget("JSONBudget");
+		//When: The budget needs to be converted to JSON format for wider use
+		//Then: The budget will convert to JSON
+		//See console for output
+		System.out.print(BudgetToJSON.convertToBudginator(testBudget));
+	}
+
+}
